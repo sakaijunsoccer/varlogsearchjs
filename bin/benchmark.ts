df92ba73c08bfaa1c919004167b43e5687373b7c
sakaijunsoccer <sakaijunsoccer@gmail.com>
2023-02-08 19:24:05 -1000

    Set 16K for default buffer

--- bin/benchmark.ts
+++ bin/benchmark.ts
@@ -4,20 +4,38 @@ import EventLog from "../src/models/eventLog"
 const benchmarkEventLog = (filename: string, keyword: string, limit: number) => {
 
     const buffer_size = 16;
-    const start = (new Date()).getTime() / 1000;
-    const eventLog = new EventLog(filename, buffer_size);
-    const results = eventLog.search([keyword], limit);
-    const end = (new Date()).getTime() / 1000;
-    console.log(`time: ${end - start} found: ${results.length} buffer size: ${buffer_size}`);
-    if (results.length !== 10000) throw new Error(`${results.length}`);
+    let start = (new Date()).getTime() / 1000;
+    const eventLog1 = new EventLog(filename, buffer_size);
+    const result1 = eventLog1.search([keyword], limit);
+    let end = (new Date()).getTime() / 1000;
+    console.log(`time: ${end - start} found: ${result1.length} buffer size: ${buffer_size}`);
 
     const buffer_size2 = 4*1024;
-    const start2 = (new Date()).getTime() / 1000;
+    start = (new Date()).getTime() / 1000;
     const eventLog2 = new EventLog(filename, buffer_size2);
-    const results2 = eventLog2.search([keyword], limit);
-    const end2 = (new Date()).getTime() / 1000;
-    console.log(`time: ${end2 - start2} found: ${results2.length} buffer size: ${buffer_size2}`);
-    if (results2.length !== 10000) throw new Error(`${results2.length}`);
+    const result2 = eventLog2.search([keyword], limit);
+    end = (new Date()).getTime() / 1000;
+    console.log(`time: ${end - start} found: ${result2.length} buffer size: ${buffer_size2}`);
+
+    const buffer_size3 = 16*1024;
+    start = (new Date()).getTime() / 1000;
+    const eventLog3 = new EventLog(filename, buffer_size3);
+    const result3 = eventLog3.search([keyword], limit);
+    end = (new Date()).getTime() / 1000;
+    console.log(`time: ${end - start} found: ${result3.length} buffer size: ${buffer_size3}`);
+
+    const buffer_size4 = 64*1024;
+    start = (new Date()).getTime() / 1000;
+    const eventLog4 = new EventLog(filename, buffer_size4);
+    const result4 = eventLog4.search([keyword], limit);
+    end = (new Date()).getTime() / 1000;
+    console.log(`time: ${end - start} found: ${result4.length} buffer size: ${buffer_size4}`);
+
+    for (const x in [result2, result3, result4]) {
+        if (result1.toString() === x.toString()){
+          throw new Error("invalid");
+        }
+    }
 };
 
 program
--- src/models/eventLog.ts
+++ src/models/eventLog.ts
@@ -1,6 +1,6 @@
 import fs from 'fs';
 
-const defaultBufferSize = 16;
+const defaultBufferSize = 16 * 1024;
 const defaultFindEventNum = 5;
 const defaultTimeOut = 5;
 
