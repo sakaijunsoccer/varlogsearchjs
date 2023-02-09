99a4acd32fb0fbd5049ce67e8df205bd3495371e
sakaijunsoccer <sakaijunsoccer@gmail.com>
2023-02-09 09:31:08 -1000

    Add node cluster for handling heavy cpu bound

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,33 @@
+import os from 'os'
+import cluster from 'cluster';
+
 import dotenv from 'dotenv';
 import swaggerUi from "swagger-ui-express";
 import swaggerJSDoc from "swagger-jsdoc";
 
 import app from './app'
 
-dotenv.config();
-const port = process.env.PORT;
+const numCPUs = os.cpus().length;
+if (cluster.isPrimary) {
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+} else {
+  dotenv.config();
+  const port = process.env.PORT;
 
-const options = {
-  swaggerDefinition: {
-    info: {
-      title: "Var log serach JS",
-      version: "1.0.0"
-    }
-  },
-  apis: ['./**/*.ts']
-};
-app.use("/docs/api/v1/", swaggerUi.serve, swaggerUi.setup(swaggerJSDoc(options)));
+  const options = {
+    swaggerDefinition: {
+      info: {
+        title: "Var log serach JS",
+        version: "1.0.0"
+      }
+    },
+    apis: ['./**/*.ts']
+  };
+  app.use("/docs/api/v1/", swaggerUi.serve, swaggerUi.setup(swaggerJSDoc(options)));
 
-app.listen(port, () => {
-  console.log(`️[server]: Server is running at http://localhost:${port}`);
-});
+  app.listen(port, () => {
+    console.log(`️[server]: Server is running at http://localhost:${port}`);
+  });
+}
